1d4a29034eaa05ef951badb211d373b35d5810a8
Josie Bealle <beallej@carleton.edu>
2015-04-10 21:41:10 -0500

    added project

---
+++ dearGod/dearGod/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  dearGod
+//
+//  Created by Josie Bealle on 10/04/2015.
+//  Copyright (c) 2015 Josie Bealle. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
