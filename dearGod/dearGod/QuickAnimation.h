6184eb29ef2911bd7e290803adfe718be9d1f649
KurtGodel <mtredding@yahoo.com>
2015-04-11 11:13:21 -0500

    changed QuickAnimation to obj-c

---
+++ dearGod/dearGod/QuickAnimation.h
@@ -0,0 +1,13 @@
+//
+//  QuickAnimation.h
+//  dearGod
+//
+//  Created by Thomas Redding on 4/11/15.
+//  Copyright (c) 2015 Josie Bealle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuickAnimation : UIImageView
+
+@end
---
+++ dearGod/dearGod/QuickAnimation.m
@@ -0,0 +1,13 @@
+//
+//  QuickAnimation.m
+//  dearGod
+//
+//  Created by Thomas Redding on 4/11/15.
+//  Copyright (c) 2015 Josie Bealle. All rights reserved.
+//
+
+#import "QuickAnimation.h"
+
+@implementation QuickAnimation
+
+@end
