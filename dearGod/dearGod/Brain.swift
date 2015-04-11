7a028402e56769f2270d7c70b61519f0dac395f4
KurtGodel <mtredding@yahoo.com>
2015-04-11 09:09:58 -0500

    in theory we can now send NSData to the server

--- dearGod/dearGod/Brain.swift
+++ dearGod/dearGod/Brain.swift
@@ -15,6 +15,17 @@ class Brain {
         converter = JSONConverter()
     }
     
+    /*
+    * This method is called after the app is opened. We are using it to test code. Remove this method before deployment!
+    */
+    func test() {
+        sendHTTPRequest("http://deargod.herokuapp.com/api/questions")
+    }
+    
+    /*
+    * This method sends an HTTP request (requestString) and runs processHTTPRequest on the response.
+    * @param the string to send to the server
+    */
     func sendHTTPRequest(requestString: String) {
         let url = NSURL(string: requestString)
         
@@ -25,6 +36,43 @@ class Brain {
         task.resume()
     }
     
+    /*
+    * This method sends an HTTP request (requestData) and runs processHTTPRequest on the response.
+    * @param the string to send to the server
+    */
+    func sendHTTPRequest(requestData: NSData) {
+        var requestString : NSString = NSString(data: requestData, encoding: NSUTF8StringEncoding)!
+        let url = NSURL(string: requestString as String)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            self.processHTTPRequest(data)
+        }
+        
+        task.resume()
+    }
+    
+    /*
+    * This method sends an HTTP request (requestData) and runs processHTTPRequest on the response.
+    * @param the string to send to the server
+    */
+    func sendHTTPRequest(requestString: String, requestData: NSData) {
+        var dataToAppend : NSString = NSString(data: requestData, encoding: NSUTF8StringEncoding)!
+        var httpRequestString : String = requestString + (dataToAppend as String)
+        let url = NSURL(string: httpRequestString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            self.processHTTPRequest(data)
+        }
+        
+        task.resume()
+    }
+    
+    
+    
+    /*
+    * This method deals with responses from the server.
+    * @param response the data received from the server
+    */
     func processHTTPRequest(response: NSData) {
         var responseAsAString = NSString(data: response, encoding: NSUTF8StringEncoding)!
         
@@ -32,30 +80,30 @@ class Brain {
         var dic : NSArray = converter.JSONToDic(response);
     }
     
-    func sendNotification(message: String) {
-        // todo
+    /*
+    * This method returns the contents of the given file
+    * @param fileName the name of the file you wish to read (excluding the extension)
+    * @param fileExtension that file's extension
+    * @param utf8 the encoding of the file (leave this blank for our purposes)
+    * @return the contents of the file as a string
+    */
+    func openFile (fileName: String, fileExtension: String, utf8: NSStringEncoding = NSUTF8StringEncoding) -> String? {
+        let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: fileExtension)
+        var error: NSError?
+        return NSFileManager().fileExistsAtPath(filePath!) ? String(contentsOfFile: filePath!, encoding: utf8, error: &error)! : nil
     }
     
-    func test() {
-        /*
-        var c = intToChar(1024)
-        println("{" + String(c) + "}")
-        println("{" + String(charToInt(c)) + "}")
-        
-        
-        let contents = openFile("userCode", fileExtension: "txt")
-        print("(")
-        print(contents)
-        println(")")
-        
-        saveFile("userCode", fileExtension: "txt", "apples are better")
-        
-        let contents2 = openFile("userCode", fileExtension: "txt")
-        print("[")
-        print(contents2)
-        println("]")
-        */
-        sendHTTPRequest("http://deargod.herokuapp.com/api/questions")
+    /*
+    * @param fileName the name of the file you wish to write to
+    * @param fileExtension that file's extension
+    * @param content the string to twrite to file
+    * @param utf8 the encoding of the file (leave this blank for our purposes)
+    * @return whether the write was successful
+    */
+    func saveFile (fileName: String, fileExtension: String, _ content: String, utf8: NSStringEncoding = NSUTF8StringEncoding) -> Bool {
+        var error: NSError?
+        let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: fileExtension)
+        return content.writeToFile(filePath!, atomically: true, encoding: utf8, error: &error)
     }
     
     func intToChar(input: Int) -> Character {
@@ -66,17 +114,5 @@ class Brain {
         let scalars = String(input).unicodeScalars;
         var integer = scalars[scalars.startIndex].value;
         return Int(integer);
-    }
-    
-    func openFile (fileName: String, fileExtension: String, utf8: NSStringEncoding = NSUTF8StringEncoding) -> String? {
-        let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: fileExtension)
-        var error: NSError?
-        return NSFileManager().fileExistsAtPath(filePath!) ? String(contentsOfFile: filePath!, encoding: utf8, error: &error)! : nil
-    }
-    
-    func saveFile (fileName: String, fileExtension: String, _ content: String, utf8: NSStringEncoding = NSUTF8StringEncoding) -> Bool {
-        var error: NSError?
-        let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: fileExtension)
-        return content.writeToFile(filePath!, atomically: true, encoding: utf8, error: &error)
     }
 }
