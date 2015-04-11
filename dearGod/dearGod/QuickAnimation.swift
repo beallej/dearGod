b0f466a1117ec08bf8582bb437f515b8f8906241
KurtGodel <mtredding@yahoo.com>
2015-04-11 10:35:37 -0500

    starting the QuickAnimation class

---
+++ dearGod/dearGod/QuickAnimation.swift
@@ -0,0 +1,13 @@
+//
+//  QuickAnimation.swift
+//  dearGod
+//
+//  Created by Thomas Redding on 4/11/15.
+//  Copyright (c) 2015 Josie Bealle. All rights reserved.
+//
+
+import Foundation
+
+class QuickAnimation {
+    // TODO
+}
