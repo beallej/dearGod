1767d844f974a908e605ca5eb91d590f0308be4e
Josie Bealle <beallej@carleton.edu>
2015-04-11 09:04:04 -0500

    globals

---
+++ dearGod/dearGod/Globals.swift
@@ -0,0 +1,23 @@
+//
+//  Globals.swift
+//  dearGod
+//
+//  Created by Josie Bealle on 11/04/2015.
+//  Copyright (c) 2015 Josie Bealle. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Singleton {
+    var backgroundColor: UIColor = UIColor(red: 0.85, green: 0.85, blue: 1.0, alpha: 1.0)
+    var brain: Brain = Brain()
+    var holyPoints: String
+    
+    init() {
+        holyPoints = brain.openFile("holyPoints", fileExtension: "txt")!
+
+    }
+}
+let sharedData = Singleton()
