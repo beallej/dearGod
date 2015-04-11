b9d0972b4d19f9b59a32cdb8a3be688dbbeeb01b
Josie Bealle <beallej@carleton.edu>
2015-04-11 02:23:12 -0500

    finally works

--- dearGod/dearGod/AnswerViewController.swift
+++ dearGod/dearGod/AnswerViewController.swift
@@ -2,16 +2,17 @@
 //  AnswerViewController.swift
 //  dearGod
 //
-//  Created by Josie Bealle on 10/04/2015.
+//  Created by Josie Bealle on 11/04/2015.
 //  Copyright (c) 2015 Josie Bealle. All rights reserved.
 //
 
 import UIKit
 
 class AnswerViewController: UIViewController {
-    
+    @IBOutlet weak var timer: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -19,6 +20,6 @@ class AnswerViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     
 }
