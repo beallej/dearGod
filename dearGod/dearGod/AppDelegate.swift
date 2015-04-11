4eb7e24d2d5bf8d6d899af73ed45e5250bffe915
KurtGodel <mtredding@yahoo.com>
2015-04-11 00:32:21 -0500

    Can now 1. convert from an integer to a character and vice-versa 2. read and write to files in the app package

--- dearGod/dearGod/AppDelegate.swift
+++ dearGod/dearGod/AppDelegate.swift
@@ -12,10 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var thomasTestVar: IosCommunicator?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        thomasTestVar = IosCommunicator()
+        thomasTestVar!.test()
         return true
     }
 
