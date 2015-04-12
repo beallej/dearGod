68a70ab31bd512deb5fe407e4be868f39096dd1e
wanwanorange <yaow@carleton.edu>
2015-04-11 21:39:47 -0500

    adding textview abilities

--- dearGod/dearGod/MainViewController.swift
+++ dearGod/dearGod/MainViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class MainViewController: UIViewController {
 
@@ -17,7 +18,12 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        var question = "Man"
+        var answer = "Yo mama is the best biathohfa ever don't you ever forget it"
+        displayQA(question, answer:answer)
+        var question2 = "Who's the prettiest girl in the world? Jesus won't you count on me? Last time I met you"
+        var answer2 = "Yo mama"
+        displayQA(question2, answer:answer2)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "checkWithBrainForTableContentsMethod:", name: "checkWithBrainForTableContents", object: nil);
         
         //sharedData.brain.getAllQuestions()
@@ -54,6 +60,36 @@ class MainViewController: UIViewController {
     
     func checkWithBrainForTableContentsMethod(notification: NSNotification) {
         NSLog("Fire!");
+    }
+    
+    var space = 70
+    var lengthq = 30
+    var lengtha = 30
+    func displayQA(question: String, answer: String){
+        var q = question
+        var a = answer
+        if (count(q)>40){
+            lengthq = Int(count(q)/25)*30
+        }
+        if (count(a)>40){
+            lengtha = Int(count(a)/25)*30
+        }
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        var screenWidth = Int(screenSize.width)
+        var questView: UITextView = UITextView (frame: CGRect(x: 10, y: space, width: screenWidth-20, height: lengthq))
+        questView.font = UIFont (name: "Avenir Book", size: 18)
+        self.view.addSubview(questView)
+        questView.text=q
+        questView.editable = false
+        space += Int(lengthq + 5)
+        var ansView: UITextView = UITextView (frame: CGRect(x: 10, y: space, width: screenWidth-20, height: lengtha))
+        ansView.font = UIFont (name: "Avenir Book", size: 18)
+        self.view.addSubview(ansView)
+        ansView.text=a
+        ansView.editable = false
+        space += Int(lengtha + 10)
+        lengthq = 30
+        lengtha = 30
     }
 
 
